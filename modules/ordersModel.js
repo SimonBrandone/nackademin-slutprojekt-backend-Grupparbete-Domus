75d8be11dc68a122e5ecb3c8b3c9a3665c823363
johannesandersson88 <70512019+johannesandersson88@users.noreply.github.com>
2021-05-20 15:22:41 +0200

    Orders
    Orders Function tillagd

--- modules/ordersModel.js
+++ modules/ordersModel.js
@@ -1,15 +1,28 @@
 const mongoose = require('mongoose');
 
+
+
 const orderSchema = mongoose.Schema({
     _id: String,
-    timeStamp: Date.now(),
-    status: String,
-    items: [{
-        type: mongoose.Schema.Types.Array
-    }],
-    orderValue: String
-})
+    status: {
+        type: String,
+        default: 'In process'
+    },
+    timestamps: {
+        type: Date,
+        default: Date.now
+    },
+    items: {
+        type: mongoose.Schema.Types.Array,
+        ref: 'Products'
+    },
+    orderValue: Number
+},
+
+)
 
 const Order = mongoose.model('Order', orderSchema)
 
 module.exports = Order
+
+// { timestamps: true}
--- routes/orders.js
+++ routes/orders.js
@@ -1,13 +1,37 @@
-const { Router } = require('express');
 const express = require('express')
-const orders = express.Router();
+const router = express.Router();
 const Order = require('../modules/ordersModel');
 
+const products = require('../assets/products.json');
+const mongoose = require('mongoose');
 
-orders.post('/api/orders', (req, res) => {
+
+
+router.post('/api/orders', async (req, res) => {
+
+
+
+
+
+    console.log(req.body)
+    let newOrder = new Order({
+        status: req.body.status,
+        timestamp: new Date(),
+        items: products[0]._id,
+        _id: new mongoose.Types.ObjectId(),
+        orderValue: products[0].price
+
+    })
+
+    try {
+        newOrder.save()
+        res.send('Order Created')
+    } catch (err) {
+        res.send(err)
+    }
 
 })
 
 
 
-module.exports = orders
+module.exports = router
