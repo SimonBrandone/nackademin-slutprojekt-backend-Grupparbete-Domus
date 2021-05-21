74c98b2517b1b6c8b4772fe28223fba0009b823c
johannesandersson88 <70512019+johannesandersson88@users.noreply.github.com>
2021-05-21 14:53:35 +0200

    Update to master
    Update J to Master

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
+// How to start use data from .env
+require('dotenv').config()
+
 // Import all Routes
 const routerJwt = require('./routes/jwt')
 const routerUser = require('./routes/users')
-const routerOrder = require('./routes/orders')
+// const routerOrder = require('./routes/orders')
 const routerProduct = require('./routes/products')
-
-// How to start use data from .env
-require('dotenv').config()
 
 // Uppkoppling till db
 const express = require('express')
@@ -20,18 +20,11 @@ const db = mongoose.connection
 db.on('error', (err) => { console.error(err) })
 db.once('open', () => { console.log("Db ansluten.") })
 
-
-// Cookies 
-const cookieParser = require('cookie-parser')
-app.use(cookieParser())
-
 // Middleware
-
 
 // This one to be able to read req.body better.
 app.use(express.urlencoded({ extended: true }))
-app.use(express.json());
-
+app.use(express.json())
 
 // This to use the frontend files.
 app.use(express.static('public'))
@@ -39,8 +32,8 @@ app.use(express.static('public'))
 // This to use different Routes.
 app.use(routerJwt)
 app.use(routerUser)
-app.use(routerOrder)
+// app.use(routerOrder)
 app.use(routerProduct)
 
 app.listen(process.env.PORT || 5000, () => console.log("It's running birch!"))
-module.exports = app
+// module.exports = app
--- routes/orders.js
+++ routes/orders.js
@@ -1,31 +1,30 @@
 const express = require('express')
 const router = express.Router();
 const Order = require('../modules/ordersModel');
-
-const products = require('../assets/products.json');
 const mongoose = require('mongoose');
+
 
 
 
 router.post('/api/orders', async (req, res) => {
 
-
-
-    console.log(req.body)
+    // Skapar nu order enligt orderSchema. 
     let newOrder = new Order({
         status: req.body.status,
         timestamp: new Date(),
-        items: products[0]._id,
+        items: req.body.items,
         _id: new mongoose.Types.ObjectId(),
-        orderValue: products[0].price
-
+        orderValue: req.body.items.price
     })
-
+    // Försöker spara, Order create tillbaka om det fungerar. 
+    // Ska till mer validering nu. 
     try {
+        //Sparas till databasen
         newOrder.save()
         res.send('Order Created')
         res.json('order created')
     } catch (err) {
+        // Om det blir fel.
         res.send(err)
     }
 
