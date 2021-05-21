b760dbc6fe02eebb2cbbd060ce1d3832fd33217e
Simon.Brandone <simon.brandone@utb.ecutbildning.se>
2021-05-21 18:39:40 +0200

    Update productsModel.js

--- modules/productsModel.js
+++ modules/productsModel.js
@@ -1,39 +1,8 @@
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
-/* const productsSchema = mongoose.Schema({
-    _id: String,
-    title: {
-        type: String,
-    },
-    shortDesc: {
-        type: String,
-    },
-    shortDesc: {
-        type: String,
-    },
-    longDesc: {
-        type: String,
-    },
-    price: Number
-    },
-
-)
- */
-
 const Products = mongoose.model('products', 
                new Schema({}), 
                'products');
 
-/* const productsSchema = new mongoose.Schema({ title: String, price: String, id: Number}, 
-    { collection : 'products' }) */
-
-
-// const Products = mongoose.model('product', productsSchema)
-
 module.exports = Products
-
-/* 
-new Schema({ url: String, text: String, id: Number}, 
-    { collection : 'question' });  */
